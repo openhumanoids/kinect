921cbf16af5cfd1720f3cee3b4cf4a9d4d1e8f01
albert <albert@d41ffffc-f2b2-4b44-b683-edcb61ded693>
2010-12-14 19:12:02 +0000

    spacing

--- src/kinect-lcm/main.c
+++ src/kinect-lcm/main.c
@@ -171,15 +171,15 @@ populate_status_t(state_t* state, kinect_status_t* msg, int64_t timestamp)
   msg->led_status = state->current_led;
 
   switch(state->tilt_state->tilt_status) {
-  case TILT_STATUS_STOPPED:
-    msg->tilt_status = KINECT_STATUS_T_TILT_STATUS_STOPPED;
-    break;
-  case TILT_STATUS_LIMIT:
-    msg->tilt_status = KINECT_STATUS_T_TILT_STATUS_LIMIT;
-    break;
-  case TILT_STATUS_MOVING:
-    msg->tilt_status = KINECT_STATUS_T_TILT_STATUS_MOVING;
-    break;
+    case TILT_STATUS_STOPPED:
+      msg->tilt_status = KINECT_STATUS_T_TILT_STATUS_STOPPED;
+      break;
+    case TILT_STATUS_LIMIT:
+      msg->tilt_status = KINECT_STATUS_T_TILT_STATUS_LIMIT;
+      break;
+    case TILT_STATUS_MOVING:
+      msg->tilt_status = KINECT_STATUS_T_TILT_STATUS_MOVING;
+      break;
   }
   msg->tilt_status = state->tilt_state->tilt_status;
 }
@@ -191,117 +191,108 @@ rate_t* rate_new(double target_hz)
   return rt;
 }
 
-void rate_destroy(rate_t* rate){
-  if(rate!=NULL){
-    free(rate);
-  }
+void rate_destroy(rate_t* rate)
+{
+  free(rate);
 }
 
-int rate_check(rate_t* rate) {
-  if(rate ==NULL){
-    return 1;
-  }
+int rate_check(rate_t* rate) 
+{
   // check the current time
   int64_t c_utime = timestamp_now();
 
   // compute the framerate if we were to publish an image
   int64_t dt = c_utime - rate->last_tick;
 
-  // if the potential framerate is too high, don't publish, and return 0
   double p_framerate = alpha * (1.0 * 1e6 / dt)  + (1 - alpha) * rate->current_hz;
-  //dbg("C: %f, P:%f \n", rate->current_hz, p_framerate);
   if(p_framerate > rate->target_hz){
+    // if the potential framerate is too high, don't publish, and return 0
     return 0;
-  }
-  else{
+  } else {
+    // otherwise, update current_hz with a exponential moving average, and return 1
     rate->current_hz = p_framerate;
     rate->last_tick = c_utime;
     return 1;
   }
-  // otherwise, update current_hz with a exponential moving average, and return 1
 }
 
 void cmd_cb(const lcm_recv_buf_t *rbuf __attribute__((unused)), 
-			const char *channel __attribute__((unused)), 
-			const kinect_cmd_t *msg,
-			void *user)
+      const char *channel __attribute__((unused)), 
+      const kinect_cmd_t *msg,
+      void *user)
 {
   state_t *self = (state_t *)user;
-  
+
   static kinect_cmd_t *static_msg = NULL;
-  
+
   if(static_msg !=NULL)
     kinect_cmd_t_destroy(static_msg);
   static_msg = kinect_cmd_t_copy(msg);
 
-  if(static_msg->type == KINECT_CMD_T_SET_TILT){
-	dbg("Received tilt command; Angle : %d\n", static_msg->tilt_degree);
-	self->freenect_angle = static_msg->tilt_degree;
+  if(static_msg->type == KINECT_CMD_T_SET_TILT) {
+    dbg("Received tilt command; Angle : %d\n", static_msg->tilt_degree);
+    self->freenect_angle = static_msg->tilt_degree;
 
-	if(self->freenect_angle > 30)
-	  self->freenect_angle = 30;
-	else if(self->freenect_angle < -20)
-	  self->freenect_angle = -20;
-	
-	freenect_set_tilt_degs(self->f_dev,self->freenect_angle);
-  }
-  else if(static_msg->type == KINECT_CMD_T_SET_LED){
-	//check if between 0 and 6
-	if( static_msg->led_status >=0 && static_msg->led_status <=6){
-	  freenect_set_led(self->f_dev, static_msg->led_status);    
-	}
-  }
-  else if(static_msg->type == KINECT_CMD_T_SET_DEPTH_DATA_FORMAT){
-	if( static_msg->depth_data_format >=0 && static_msg->depth_data_format < 4){
-	  self->requested_depth_format = static_msg->depth_data_format;
-	}	
-  }
+    if(self->freenect_angle > 30)
+      self->freenect_angle = 30;
+    else if(self->freenect_angle < -20)
+      self->freenect_angle = -20;
 
-  else if(static_msg->type == KINECT_CMD_T_SET_IMAGE_DATA_FORMAT){
-	if( static_msg->image_data_format == KINECT_CMD_T_VIDEO_RGB ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_BAYER ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_IR_8BIT ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_IR_10BIT ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_IR_10BIT_PACKED ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_YUV_RGB ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_YUV_RAW ||
-		static_msg->image_data_format == KINECT_CMD_T_VIDEO_JPEG){
-	  self->requested_image_format = static_msg->image_data_format;
-	}	
+    freenect_set_tilt_degs(self->f_dev,self->freenect_angle);
+  } else if(static_msg->type == KINECT_CMD_T_SET_LED) {
+    //check if between 0 and 6
+    if( static_msg->led_status >=0 && static_msg->led_status <=6) {
+      freenect_set_led(self->f_dev, static_msg->led_status);
+    }
+  } else if(static_msg->type == KINECT_CMD_T_SET_DEPTH_DATA_FORMAT) {
+    if( static_msg->depth_data_format >=0 && static_msg->depth_data_format < 4) {
+      self->requested_depth_format = static_msg->depth_data_format;
+    } 
+  } else if(static_msg->type == KINECT_CMD_T_SET_IMAGE_DATA_FORMAT) {
+    if( static_msg->image_data_format == KINECT_CMD_T_VIDEO_RGB ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_BAYER ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_IR_8BIT ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_IR_10BIT ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_IR_10BIT_PACKED ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_YUV_RGB ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_YUV_RAW ||
+        static_msg->image_data_format == KINECT_CMD_T_VIDEO_JPEG) {
+      self->requested_image_format = static_msg->image_data_format;
+    } 
   }
 }
 
 void depth_cb(freenect_device *dev, void *data, uint32_t timestamp)
 {
   state_t* state = (state_t*) freenect_get_user(dev);
-  if(!rate_check(state->depth_rate)){
+  if(!rate_check(state->depth_rate)) {
     return;
   }
   int64_t host_utime = timestamp_now();
   state->depth_msg.timestamp = timestamp_sync(state->clocksync, timestamp, host_utime);
 
   switch(state->current_depth_format) {
-  case FREENECT_DEPTH_11BIT:
-    state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_11BIT_SIZE;
-    state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_11BIT;
-    break;
-  case FREENECT_DEPTH_10BIT:
-    state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_10BIT_SIZE;
-    state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_10BIT;
-    break;
-  case FREENECT_DEPTH_11BIT_PACKED:
-    state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_11BIT_PACKED_SIZE;
-    state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_11BIT_PACKED;
-    break;
-  case FREENECT_DEPTH_10BIT_PACKED:
-    state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_10BIT_PACKED_SIZE;
-    state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_10BIT_PACKED;
-    break;
-  default:
-    state->depth_msg.depth_data_nbytes = 0;
-    state->depth_msg.depth_data_format = 0; // TODO spew warning
+    case FREENECT_DEPTH_11BIT:
+      state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_11BIT_SIZE;
+      state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_11BIT;
+      break;
+    case FREENECT_DEPTH_10BIT:
+      state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_10BIT_SIZE;
+      state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_10BIT;
+      break;
+    case FREENECT_DEPTH_11BIT_PACKED:
+      state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_11BIT_PACKED_SIZE;
+      state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_11BIT_PACKED;
+      break;
+    case FREENECT_DEPTH_10BIT_PACKED:
+      state->depth_msg.depth_data_nbytes = FREENECT_DEPTH_10BIT_PACKED_SIZE;
+      state->depth_msg.depth_data_format = KINECT_DEPTH_DATA_T_DEPTH_10BIT_PACKED;
+      break;
+    default:
+      state->depth_msg.depth_data_nbytes = 0;
+      state->depth_msg.depth_data_format = 0; // TODO spew warning
 
-    break;
+      break;
   }
 
   assert(state->depth_msg.depth_data_nbytes < state->depth_buf_size);
@@ -324,38 +315,38 @@ void image_cb(freenect_device *dev, void *data, uint32_t timestamp)
   state->image_msg.timestamp = timestamp_sync(state->clocksync, timestamp, host_utime);
 
   switch(state->current_image_format) {
-  case FREENECT_VIDEO_RGB:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_RGB_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_RGB;
-    break;
-  case FREENECT_VIDEO_BAYER:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_BAYER_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_BAYER;
-    break;
-  case FREENECT_VIDEO_YUV_RGB:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_YUV_RGB_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_YUV_RGB;
-    break;
-  case FREENECT_VIDEO_YUV_RAW:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_YUV_RAW_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_YUV_RAW;
-    break;
-  case FREENECT_VIDEO_IR_8BIT:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_IR_8BIT_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_IR_8BIT;
-    break;
-  case FREENECT_VIDEO_IR_10BIT:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_IR_10BIT_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_IR_10BIT;
-    break;
-  case FREENECT_VIDEO_IR_10BIT_PACKED:
-    state->image_msg.image_data_nbytes =  FREENECT_VIDEO_IR_10BIT_PACKED_SIZE;
-    state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_IR_10BIT_PACKED;
-    break;
-  default:
-    state->image_msg.image_data_nbytes = 0; // TODO spew warning
-    state->image_msg.image_data_format = -1;
-    break;
+    case FREENECT_VIDEO_RGB:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_RGB_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_RGB;
+      break;
+    case FREENECT_VIDEO_BAYER:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_BAYER_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_BAYER;
+      break;
+    case FREENECT_VIDEO_YUV_RGB:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_YUV_RGB_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_YUV_RGB;
+      break;
+    case FREENECT_VIDEO_YUV_RAW:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_YUV_RAW_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_YUV_RAW;
+      break;
+    case FREENECT_VIDEO_IR_8BIT:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_IR_8BIT_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_IR_8BIT;
+      break;
+    case FREENECT_VIDEO_IR_10BIT:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_IR_10BIT_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_IR_10BIT;
+      break;
+    case FREENECT_VIDEO_IR_10BIT_PACKED:
+      state->image_msg.image_data_nbytes =  FREENECT_VIDEO_IR_10BIT_PACKED_SIZE;
+      state->image_msg.image_data_format = KINECT_IMAGE_DATA_T_VIDEO_IR_10BIT_PACKED;
+      break;
+    default:
+      state->image_msg.image_data_nbytes = 0; // TODO spew warning
+      state->image_msg.image_data_format = -1;
+      break;
   }
   assert(state->image_msg.image_data_nbytes < state->image_buf_size);
   memcpy(state->image_buf, data, state->image_msg.image_data_nbytes);
@@ -384,29 +375,27 @@ freenect_threadfunc(void *user_data)
 
   while (!state->die && freenect_process_events(state->f_ctx) >= 0) {
     if(state->have_img && !state->skip_img){ 
-	populate_status_t(state, &state->image_msg.status, state->image_msg.timestamp);
-	kinect_image_data_t_publish(state->lcm, state->image_channel, &state->image_msg);
-	dbg("i");
-	state->have_img = 0;
+      populate_status_t(state, &state->image_msg.status, state->image_msg.timestamp);
+      kinect_image_data_t_publish(state->lcm, state->image_channel, &state->image_msg);
+      state->have_img = 0;
     } else if(state->have_depth){
-	populate_status_t(state, &state->depth_msg.status, state->depth_msg.timestamp);
-	kinect_depth_data_t_publish(state->lcm, state->depth_channel, &state->depth_msg);
-	dbg("d");
-	state->have_depth = 0;
+      populate_status_t(state, &state->depth_msg.status, state->depth_msg.timestamp);
+      kinect_depth_data_t_publish(state->lcm, state->depth_channel, &state->depth_msg);
+      state->have_depth = 0;
     }
     if (state->requested_image_format != state->current_image_format) {
-	  dbg("Changing Image format\n");
+      dbg("Changing Image format\n");
       freenect_stop_video(state->f_dev);
       freenect_set_video_format(state->f_dev, state->requested_image_format);
       freenect_start_video(state->f_dev);
       state->current_image_format = state->requested_image_format;
     }
-	if (state->requested_depth_format != state->current_depth_format) {
-	  dbg("Changing Depth format\n");
-	  freenect_stop_depth(state->f_dev);
-	  freenect_set_depth_format(state->f_dev, state->requested_depth_format);
+    if (state->requested_depth_format != state->current_depth_format) {
+      dbg("Changing Depth format\n");
+      freenect_stop_depth(state->f_dev);
+      freenect_set_depth_format(state->f_dev, state->requested_depth_format);
       freenect_start_depth(state->f_dev);
-	  state->current_depth_format = state->requested_depth_format;
+      state->current_depth_format = state->requested_depth_format;
     }
   }
 
@@ -422,13 +411,23 @@ freenect_threadfunc(void *user_data)
   return NULL;
 }
 
+static void usage(const char* progname)
+{
+  fprintf (stderr, "Usage: %s [-v] [-c] [-d] [-p]\n\
+      Options:\n\
+      -r     [rate] Throttle publishing\n \
+      -i     Ignore images\n\
+      -h     This help message\n", g_path_get_basename(progname));
+  exit(1);
+}
+
 int main(int argc, char **argv)
 {
   state_t *state = (state_t*)calloc(1, sizeof(state_t));
 
   int c;
   double target_rate = 0;
-    
+
   state->skip_img = 0;
   state->throttle = 0;
   state->depth_rate = NULL;
@@ -437,31 +436,25 @@ int main(int argc, char **argv)
   //command line options - to throtle - to ignore image publish  
   while ((c = getopt (argc, argv, "hir:")) >= 0) {
     switch (c) {
-    case 'i': //ignore images 
-      state->skip_img = 1;
-      dbg("Skipping image publishing\n");
-      break;
-    case 'r':
-      target_rate = strtod (optarg,NULL);
-      dbg("Target Rate is : %f\n", target_rate); 
-      state->depth_rate = rate_new(target_rate);
-      state->img_rate = rate_new(target_rate);
-      state->throttle = 1;
-      break;
+      case 'i': //ignore images 
+        state->skip_img = 1;
+        dbg("Skipping image publishing\n");
+        break;
+      case 'r':
+        target_rate = strtod (optarg,NULL);
+        dbg("Target Rate is : %f\n", target_rate); 
+        state->depth_rate = rate_new(target_rate);
+        state->img_rate = rate_new(target_rate);
+        state->throttle = 1;
+        break;
 
-    //add option to select the image/depth type 
+        //add option to select the image/depth type 
 
-    case 'h':
-    case '?':
-      fprintf (stderr, "Usage: %s [-v] [-c] [-d] [-p]\n\
-                        Options:\n\
-                        -r     [rate] Throttle publishing\n	\
-                        -i     Ignore images\n\
-                        -h     This help message\n", argv[0]);
-    return 1;
+      case 'h':
+      case '?':
+        usage(argv[0]);
     }
   }
-
 
   state->freenect_thread = NULL;
   state->die = 0;
