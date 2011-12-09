8fb2b4cab8a94fb3b5ad52a18a437d9fcccc6ccf
sachih <sachih@d41ffffc-f2b2-4b44-b683-edcb61ded693>
2011-12-09 19:14:47 +0000

    adding new openni features

--- src/kinect-openni-lcm/kinectOpenniLCM.cpp
+++ src/kinect-openni-lcm/kinectOpenniLCM.cpp
@@ -9,10 +9,6 @@
 #include "openni_camera/openni_depth_image.h"
 #include "openni_camera/openni_image.h"
 
-#include <lcmtypes/kinect_depth_msg_t.h>
-#include <lcmtypes/kinect_image_msg_t.h>
-#include <lcmtypes/kinect_frame_msg_t.h>
-#include <lcmtypes/kinect_cmd_msg_t.h>
 #include <bot_core/timestamp.h>
 
 #include <zlib.h>
@@ -45,6 +41,191 @@ KinectOpenniLCM::KinectOpenniLCM(int argc, char **argv)
 
   m_lcm = bot_lcm_get_global(NULL);
 
+
+  state = (state_t*) calloc(1, sizeof(state_t));
+
+  double target_rate = INFINITY;
+
+  state->skip_img = 0;
+  state->skip_depth = 0;
+  state->throttle = 0;
+  state->capture_rate = NULL;
+
+  state->openni_thread = NULL;
+  state->die = 0;
+
+  state->jpeg_quality = 94;
+
+  // make these configurable - either/both from the command line and from outside LCM command
+  state->requested_image_format = KINECT_IMAGE_MSG_T_VIDEO_BAYER;
+  state->requested_depth_format = KINECT_DEPTH_MSG_T_DEPTH_11BIT;
+  state->current_image_format = state->requested_image_format;
+  state->current_depth_format = state->requested_depth_format;
+  int user_device_number = 0;
+  state->msg_channel = g_strdup("KINECT_FRAME");
+
+  int c;
+  char *lcm_url = NULL;
+  // command line options - to throtle - to ignore image publish  
+  while ((c = getopt(argc, argv, "hd:i:r:jq:zl:n:c:")) >= 0) {
+    switch (c) {
+    case 'i': //ignore images
+      state->requested_image_format = atoi(optarg);
+      break;
+    case 'd':
+      state->requested_depth_format = atoi(optarg);
+      break;
+    case 'j':
+      state->requested_image_format = KINECT_IMAGE_MSG_T_VIDEO_RGB_JPEG;
+      break;
+    case 'q':
+      state->jpeg_quality = atoi(optarg);
+      if (state->jpeg_quality < 0 || state->jpeg_quality > 100)
+        usage(argv[0]);
+      break;
+    case 'n':
+      user_device_number = atoi(optarg);
+      printf("attempting to open device %i\n", user_device_number);
+      break;
+    case 'z':
+      state->use_zlib = 1;
+      printf("ZLib compressing depth data\n");
+      break;
+    case 'r':
+      target_rate = strtod(optarg, NULL);
+      printf("Target Rate is : %.3f Hz\n", target_rate);
+      state->throttle = 1;
+      break;
+    case 'l':
+      lcm_url = strdup(optarg);
+      printf("Using LCM URL \"%s\"\n", lcm_url);
+      break;
+    case 'c':
+      g_free(state->msg_channel);
+      state->msg_channel = g_strdup(optarg);
+      printf("Output on LCM channel: %s\n", state->msg_channel);
+      break;
+    case 'h':
+      case '?':
+      usage(argv[0]);
+    }
+  }
+
+  const char * depthModeSting[] = {
+      "DEPTH_11BIT",
+      "DEPTH_10BIT",
+      "DEPTH_11BIT_PACKED",
+      "DEPTH_10BIT_PACKED",
+      "DEPTH_REGISTERED",
+      "DEPTH_MM",
+  };
+  switch (state->requested_depth_format) {
+  case KINECT_DEPTH_MSG_T_DEPTH_11BIT:
+    case KINECT_DEPTH_MSG_T_DEPTH_10BIT:
+    case KINECT_DEPTH_MSG_T_DEPTH_11BIT_PACKED:
+    case KINECT_DEPTH_MSG_T_DEPTH_10BIT_PACKED:
+    case KINECT_DEPTH_MSG_T_DEPTH_REGISTERED:
+    case KINECT_DEPTH_MSG_T_DEPTH_MM:
+    dbg("Depth Mode is %s\n", depthModeSting[state->requested_depth_format]);
+    break;
+  case KINECT_DEPTH_MSG_T_DEPTH_NONE:
+    case -1:
+    dbg("Depth is disabled");
+    state->requested_depth_format = KINECT_DEPTH_MSG_T_DEPTH_NONE;
+    state->skip_depth = 1;
+    break;
+  default:
+    dbg("Invalid depth format %d\n", state->requested_depth_format);
+    usage(argv[0]);
+    break;
+  }
+
+  const char * imageModeSting[] = {
+      "VIDEO_RGB",
+      "VIDEO_BAYER",
+      "VIDEO_IR_8BIT",
+      "VIDEO_IR_10BIT",
+      "VIDEO_IR_10BIT_PACKED",
+      "VIDEO_YUV_RGB",
+      "VIDEO_YUV_RAW",
+  };
+
+  switch (state->requested_image_format) {
+  case KINECT_IMAGE_MSG_T_VIDEO_RGB:
+    case KINECT_IMAGE_MSG_T_VIDEO_BAYER:
+    case KINECT_IMAGE_MSG_T_VIDEO_IR_8BIT:
+    case KINECT_IMAGE_MSG_T_VIDEO_IR_10BIT:
+    case KINECT_IMAGE_MSG_T_VIDEO_IR_10BIT_PACKED:
+    case KINECT_IMAGE_MSG_T_VIDEO_YUV_RGB:
+    case KINECT_IMAGE_MSG_T_VIDEO_YUV_RAW:
+    dbg("Image Mode is %s\n", imageModeSting[state->requested_image_format]);
+    break;
+
+  case KINECT_IMAGE_MSG_T_VIDEO_RGB_JPEG:
+    dbg("Jpeg Compressing RGB images\n");
+    break;
+  case KINECT_IMAGE_MSG_T_VIDEO_NONE:
+    case -1:
+    dbg("Image is disabled");
+    state->requested_image_format = KINECT_IMAGE_MSG_T_VIDEO_NONE;
+    state->skip_img = 1;
+    break;
+  default:
+    dbg("Invalid image format %d\n", state->requested_image_format);
+    usage(argv[0]);
+    break;
+  }
+
+  // throttling
+  state->capture_rate = (rate_t *) calloc(1, sizeof(rate_t));
+  state->capture_rate->target_hz = target_rate;
+  state->capture_rate->tick_count = 0;
+
+  //todo the width and height should be computed from the desired resolution
+
+  // allocate image and depth buffers
+  state->image_data = (uint8_t*) malloc(640 * 480 * 4);
+
+  // allocate more space for the image buffer, as we might use it for compressed data
+  state->image_buf_size = 640 * 480 * 10;
+  if (0 != posix_memalign((void**) &state->image_buf, 16, state->image_buf_size)) {
+    fprintf(stderr, "Error allocating image buffer\n");
+    exit(1);
+  }
+  state->msg.image.image_data = state->image_buf;
+  state->msg.image.width = 640; //TODO: this shouldn't be hardcoded
+  state->msg.image.height = 480;
+
+  // allocate a buffer for bayer de-mosaicing
+  state->debayer_buf_size = 640 * 480 * 4;
+  state->debayer_buf_stride = 640 * 4;
+  if (0 != posix_memalign((void**) &state->debayer_buf, 16, state->debayer_buf_size)) {
+    fprintf(stderr, "error allocating de-Bayer buffer\n");
+    exit(1);
+  }
+
+  // allocate space for unpacking depth data
+  state->depth_unpack_buf_size = 640 * 480 * sizeof(uint16_t);
+  state->depth_unpack_buf = (uint16_t*) malloc(state->depth_unpack_buf_size);
+
+  // allocate space for zlib compressing depth data
+  state->depth_compress_buf_size = 640 * 480 * sizeof(int16_t) * 4;
+  state->depth_compress_buf = (uint8_t*) malloc(state->depth_compress_buf_size);
+  state->msg.depth.depth_data = state->depth_compress_buf;
+  state->msg.depth.width = 640;
+  state->msg.depth.height = 480;
+
+  state->got_img = 0;
+  state->got_depth = 0;
+
+  state->report_rate = (rate_t *) calloc(1, sizeof(rate_t));
+  state->report_rate->target_hz = 0.5;
+  state->report_rate->tick_count = 0;
+
+  state->last_timestamp = 0;
+
+
+
   SetupDevice(deviceId);
 
   new_data = false;
@@ -56,6 +237,48 @@ KinectOpenniLCM::~KinectOpenniLCM()
     m_device->stopDepthStream ();
     m_device->stopImageStream ();
   }
+}
+
+void KinectOpenniLCM::usage(const char* progname)
+{
+  fprintf(stderr, "Usage: %s [options]\n"
+      "\n"
+      "Options:\n"
+      "  -r RATE   Throttle publishing to RATE Hz.\n"
+      "  -d        Depth mode\n"
+      "  -i        Image mode\n"
+      "  -j        JPEG-compress RGB images\n"
+      "  -q QUAL   JPEG compression quality (0-100, default 94)\n"
+      "  -z        ZLib compress depth images\n"
+      "  -l URL    Specify LCM URL\n"
+      "  -h        This help message\n"
+      "  -n dev    Number of the device to open\n"
+      "  -c name   LCM channel\n",
+      g_path_get_basename(progname));
+
+  fprintf(stderr, "Image mode must be one of:\n"
+      "  VIDEO_RGB             = 0\n"
+      "  VIDEO_BAYER           = 1\n"
+      "  VIDEO_IR_8BIT         = 2\n"
+      "  VIDEO_IR_10BIT        = 3\n"
+      "  VIDEO_IR_10BIT_PACKED = 4\n"
+      "  VIDEO_YUV_RGB         = 5\n"
+      "  VIDEO_YUV_RAW         = 6\n"
+      "\n"
+      "  VIDEO_DISABLED        = -1\n"
+      );
+
+  fprintf(stderr, "Depth mode must be one of:\n"
+      "  DEPTH_11BIT        = 0\n"
+      "  DEPTH_10BIT        = 1\n"
+      "  DEPTH_11BIT_PACKED = 2\n"
+      "  DEPTH_10BIT_PACKED = 3\n"
+      "  DEPTH_REGISTERED   = 4\n"
+      "  DEPTH_MM           = 5\n"
+      "\n"
+      "  DEPTH_DISABLED         =-1\n"
+      );
+  exit(1);
 }
 
 void KinectOpenniLCM::SetupDevice(const std::string& deviceId)
@@ -191,7 +414,8 @@ void KinectOpenniLCM::DepthCallback (boost::shared_ptr<openni_wrapper::DepthImag
   msg.depth.uncompressed_size = msg.depth.depth_data_nbytes;
   msg.depth.depth_data = (uint8_t*)calloc(msg.depth.depth_data_nbytes, sizeof(uint8_t));
 //new uint8_t[msg.depth.depth_data_nbytes];
-    
+  //memset(msg.depth.depth_data, 128, msg.depth.depth_data_nbytes/2);    
+  //memset(&msg.depth.depth_data[msg.depth.depth_data_nbytes/2], 255, msg.depth.depth_data_nbytes/2);    
   depth_image->fillDepthImageRaw(msg.depth.width, msg.depth.height, reinterpret_cast<unsigned short*>(msg.depth.depth_data), depth_image->getWidth() * sizeof(short));
 
   kinect_frame_msg_t_publish(m_lcm, "KINECT_FRAME", &msg);
--- src/kinect-openni-lcm/kinectOpenniLCM.h
+++ src/kinect-openni-lcm/kinectOpenniLCM.h
@@ -16,6 +16,39 @@
 #include "openni_camera/openni_device.h"
 #include "openni_camera/openni_driver.h"
 
+//------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#include <zlib.h>
+#include <glib.h>
+#include <lcm/lcm.h>
+
+#if USE_JPEG_UTILS_POD
+#include <jpeg-utils/jpeg-utils.h>
+#else
+#include "jpeg-utils-ijg.h"
+#endif
+
+#include <lcmtypes/kinect_depth_msg_t.h>
+#include <lcmtypes/kinect_image_msg_t.h>
+#include <lcmtypes/kinect_frame_msg_t.h>
+
+#include "timestamp.h"
+#include "pixels.h"
+
+#define dbg(...) fprintf(stderr, __VA_ARGS__)
+
+#define alpha 0.05 //decay rate for the moving average
+//----------------------------------
+
+
 class KinectOpenniLCM
 {
  public:
@@ -29,6 +62,7 @@ class KinectOpenniLCM
   void DepthCallback (boost::shared_ptr<openni_wrapper::DepthImage> depth_image, void* cookie);
   void startSynchronization ();
   void stopSynchronization ();
+  void usage(const char*);
 
  private:
 
@@ -39,7 +73,82 @@ class KinectOpenniLCM
   uint8_t* rgb_data;
   bool new_data;
 
+  typedef struct rate_t {
+  double target_hz;
+  double current_hz;
+  int64_t last_tick;
+  int64_t tick_count;
+  } rate_t;
+
+  void rate_destroy(rate_t* rate);
+
+  /**
+   * returns: 1 if an image should be published.  0 if not
+   */
+  int rate_check(rate_t* rate);
+
+  typedef struct _state_t {
+  GThread* openni_thread;
+  volatile int die;
+
+  int8_t requested_image_format;
+  int8_t current_image_format;
+
+  int8_t requested_depth_format;
+  int8_t current_depth_format;
+
+  int8_t current_resolution;
+
+  uint8_t* image_data;
+
+  uint8_t* image_buf;
+  int image_buf_size;
+
+  uint8_t* debayer_buf;
+  int debayer_buf_size;
+
+  uint16_t* depth_unpack_buf;
+  int depth_unpack_buf_size;
+
+  uint8_t* depth_compress_buf;
+  int debayer_buf_stride;
+  int depth_compress_buf_size;
+
+  double accel_mks[3];
+  double tilt_radians;
+
+
+  kinect_frame_msg_t msg;
+  char* msg_channel;
+
+  int got_img;
+  int got_depth;
+
+  int skip_img;
+  int skip_depth;
+  int throttle; //1 in data skip will be published
+  timestamp_sync_state_t* clocksync;
+
+  rate_t* capture_rate;
+  rate_t* report_rate;
+
+  uint8_t* jpeg_buf;
+  int jpeg_buf_size;
+
+  int jpeg_quality;
+
+  int use_zlib;
+
+  lcm_t* lcm;
+
+  int64_t last_timestamp;
+  int64_t last_depth_timestamp;
+  int64_t last_img_timestamp;
+} state_t;
+
   boost::shared_ptr<openni_wrapper::OpenNIDevice> m_device;
+
+  state_t *state;
 };
 
 #endif
